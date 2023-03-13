4a2ab8b15e595019ba32440724c4e48de881a7e9
Sean Steimer <ssteimer@adobe.com>
2023-03-13 09:18:40 -0700

    chore: support a poster image on video blocks

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -8,8 +8,9 @@ export default async function decorate(block) {
   const a = block.querySelector('a');
   if (a) {
     const source = a.href;
+    const img = block.querySelector('img');
     block.innerHTML = `
-    <video controls>
+    <video controls ${img ? `poster="${img.src}"` : ''}>
       <source src="${source}" type="video/${source.split('.').pop()}" >
     </video>
     `;
